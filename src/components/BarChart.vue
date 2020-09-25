dca83c9fea0bd25f3433e2737b90ec73f663ee9b
Domen Gašperlin <domen.gasperlin@gmail.com>
2020-09-26 00:29:34 +0200

    Added preparations view

---
+++ src/components/BarChart.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <v-chart :options="option" style="width: auto; margin: 0 auto;" />
+  </div>
+</template>
+
+<script>
+// const moment = require('moment')
+
+export default {
+  name: "BarChart",
+  props: ['maxVal', 'currVal', 'critVal', 'title'],
+  methods: {
+    popRandomColor(){
+      if (this.currVal <= this.critVal) {
+        return '#ff0000'
+      } else {
+        return '#00ff00'
+      }
+    }
+  },
+  computed: {
+  },
+  data() {
+    return {
+      option: {
+        title: {
+          text: this.title
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+        // legend: {
+        //   orient: 'vertical',
+        //   left: 10,
+        //   data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        // },
+        grid: {
+          // left: '3%',
+          // right: '4%',
+          // bottom: '3%',
+          containLabel: true,
+          y2: 1
+        },
+        // toolbox: {
+        //   show: true,
+        //   feature: {
+        //     mark: { show: true },
+        //     dataView: { show: true, readOnly: false, title: "Podatki" },
+        //     restore: { show: true, title: "Ponastavi" },
+        //     saveAsImage: { show: true, title: "Shrani" }
+        //   }
+        // },
+        xAxis: {
+          type: 'category',
+          show: true,
+        },
+        yAxis: {
+          type: 'value',
+          show: true,
+          max: this.maxVal
+        },
+        series: [
+          {
+            name: 'zaloga',
+            type: 'bar',
+
+            showBackground: true,
+            backgroundStyle: {
+              color: 'rgba(220, 220, 220, 0.8)'
+            },
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: '30',
+                fontWeight: 'bold'
+              }
+            },
+            labelLine: {
+              show: false
+            },
+            data: [{
+              max: this.maxVal,
+              value: this.currVal,
+              itemStyle: {color: this.popRandomColor()},
+            }]
+          },
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Preparation.vue
+++ src/views/Preparation.vue
@@ -1,10 +1,57 @@
 <template>
-  <div></div>
+  <b-container>
+    <b-row>
+      <b-col></b-col>
+      <b-col></b-col>
+      <b-col></b-col>
+    </b-row>
+    <b-row>
+      <b-col md="3">
+        <bar-chart title="ZU" :max-val="10" :crit-val="3" :curr-val="2" ></bar-chart>
+      </b-col>
+      <b-col md="3">
+        <bar-chart title="Vezivo" :max-val="35" :crit-val="5" :curr-val="3.4" ></bar-chart>
+      </b-col>
+      <b-col md="3">
+        <bar-chart title="Razgrajevalo" :max-val="25" :crit-val="5" :curr-val="5.5" ></bar-chart>
+      </b-col>
+      <b-col md="3">
+        <bar-chart title="Polnilo" :max-val="25" :crit-val="5" :curr-val="5.8" ></bar-chart>
+      </b-col>
+      <b-col md="3">
+        <bar-chart title="Topilo 1" :max-val="65" :crit-val="10" :curr-val="0" ></bar-chart>
+      </b-col>
+      <b-col md="3">
+        <bar-chart title="Topilo 1" :max-val="30" :crit-val="5" :curr-val="6" ></bar-chart>
+      </b-col>
+      <b-col md="3">
+        <bar-chart title="Razgrajevalo 1" :max-val="25" :crit-val="5" :curr-val="6.5" ></bar-chart>
+      </b-col>
+      <b-col md="3">
+        <bar-chart title="Razgrajevalo 2" :max-val="15" :crit-val="3" :curr-val="4.5" ></bar-chart>
+      </b-col>
+      <b-col md="3">
+        <bar-chart title="Lubrikant" :max-val="5" :crit-val="2" :curr-val="3" ></bar-chart>
+      </b-col>
+      <b-col md="3">
+        <bar-chart title="Razgrajevalo 2" :max-val="3" :crit-val="1" :curr-val="0" ></bar-chart>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import BarChart from "@/components/BarChart";
+import moment from 'moment';
+console.log(moment)
+
+
+
 export default {
-  name: "Preparation"
+  name: "Granulation",
+  components: {BarChart},
+  computed: {
+  }
 }
 </script>
 
